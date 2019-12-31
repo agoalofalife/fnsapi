ec360d51fa6398cace2a2c8684c7ac23c2b81f1c
agoalofalife <agoalofalife@gmail.com>
2019-12-31 15:38:16 +0300

    Add abstract request class, parent class for childrens SendMessage types

---
+++ src/AbstractRequest.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace Fns;
+
+
+use Psr\SimpleCache\CacheInterface;
+use SoapClient;
+
+abstract class AbstractRequest
+{
+    private $wsdl = 'https://openapi.nalog.ru:8090/open-api/ais3/KktService/0.1?wsdl';
+    private $storage;
+    private $userToken;
+    protected $client;
+
+    public function __construct(string $userToken, CacheInterface $cache)
+    {
+        $this->storage = $cache;
+        $this->userToken = $userToken;
+        $this->client = new SoapClient(
+            $this->wsdl,
+            [
+                'stream_context' => stream_context_create(['http' => ['header' => $this->getHeaderString()]])
+            ]
+        );
+    }
+
+    private function getHeaderString() : string
+    {
+        return sprintf(
+            "FNS-OpenApi-Token:%s
+                    FNS-OpenApi-UserToken:%s",
+            $this->storage->get('temp_token'),
+            $this->userToken
+        );
+    }
+
+    abstract protected function getXml() : array;
+}
