83df943c6b816403daac803d25d0c9f87c8c61e9
agoalofalife <agoalofalife@gmail.com>
2019-12-31 15:39:15 +0300

    Add universal class SendMessage

--- src/SendMessageRequest.php
+++ src/SendMessageRequest.php
@@ -3,71 +3,46 @@ declare(strict_types=1);
 
 namespace Fns;
 
-use Psr\SimpleCache\CacheInterface;
-use SoapClient;
-
-class SendMessageRequest
+class SendMessageRequest extends AbstractRequest
 {
-    private $wsdl = 'https://openapi.nalog.ru:8090/open-api/ais3/KktService/0.1?wsdl';
-
-    /**
-     * @var string
-     */
-    private $userToken;
-    /**
-     * @var CacheInterface
-     */
-    private $storage;
-    /**
-     * @var SoapClient
-     */
-    private $client;
-    /**
-     * @var Ticket
-     */
     private $ticket;
+    private $typeRequest;
 
-    public function __construct(string $userToken, CacheInterface $cache, Ticket $ticket)
-    {
-        $this->userToken = $userToken;
-        $this->storage = $cache;
-        $this->client = new SoapClient(
-            $this->wsdl,
-            [
-                'stream_context' => stream_context_create(['http' => ['header' => $this->getHeaderString()]])
-            ]
-        );
-        $this->ticket = $ticket;
-    }
-
-    private function getHeaderString() : string
-    {
-        return sprintf(
-            "FNS-OpenApi-Token:%s
-                    FNS-OpenApi-UserToken:%s",
-            $this->storage->get('temp_token'),
-            $this->userToken
-        );
-    }
-
-    private function xmlCheckTicket() : array
+    protected function getXml() : array
     {
         return [[
             'Message' => [
                 'any' =>
-                    "<CheckTicketRequest 
+                    "<{$this->typeRequest}Request 
                     xmlns=\"urn://x-artefacts-gnivc-ru/ais3/kkt/KktTicketService/types/1.0\"
                     xmlns:tns=\"urn://x-artefacts-gnivc-ru/inplat/servin/OpenApiAsyncMessageConsumerService/types/1.0\">
-                        <tns:CheckTicketInfo>
+                        <tns:{$this->typeRequest}Info>
                                 {$this->ticket->asXml()}
-                        </tns:CheckTicketInfo>
-                    </CheckTicketRequest>"
+                        </tns:{$this->typeRequest}Info>
+                    </{$this->typeRequest}Request>"
             ]
         ]];
     }
 
-    public function checkTicketRequest() : string
+    private function setTypeRequest(string $type)
     {
-        return $this->client->__soapCall("SendMessage", $this->xmlCheckTicket())->MessageId;
+        $this->typeRequest = $type;
+    }
+
+    public function setTicket(Ticket $ticket)
+    {
+        $this->ticket = $ticket;
+    }
+
+    public function checkTicket() : string
+    {
+        $this->setTypeRequest('CheckTicket');
+        return $this->client->__soapCall("SendMessage", $this->getXml())->MessageId;
+    }
+
+    public function getTicket() : string
+    {
+        $this->setTypeRequest('GetTicket');
+        return $this->client->__soapCall("SendMessage", $this->getXml())->MessageId;
     }
 }
