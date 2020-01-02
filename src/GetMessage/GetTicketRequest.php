b0fc15077e508ce511a622aa59e32489cda9c2a7
agoalofalife <agoalofalife@gmail.com>
2020-01-02 20:42:25 +0300

    get ticket request add

--- src/GetMessage/GetTicketRequest.php
+++ src/GetMessage/GetTicketRequest.php
@@ -3,10 +3,54 @@ declare(strict_types=1);
 
 namespace Fns\GetMessage;
 
-class GetTicketRequest extends GetMessageRequest
-{
-    public function getTicketInfo()
-    {
+use Fns\Contracts\RequestsManager;
+use Fns\Contracts\ResponseSendMessage;
+use Fns\Contracts\SetTimeoutHandler;
+use Fns\Contracts\TimeoutStrategyHandler;
 
+class GetTicketRequest extends GetMessageRequest implements RequestsManager, SetTimeoutHandler
+{
+    private $response;
+
+    /**
+     * @var ResponseSendMessage
+     */
+    private $xmlResponse;
+    private $strategyTimeout;
+
+    public function setTimeoutStrategy(TimeoutStrategyHandler $strategyHandler)
+    {
+        $this->strategyTimeout = $strategyHandler;
+    }
+
+    public function send()
+    {
+        $this->strategyTimeout->handleTimeout();
+        $this->xmlResponse = simplexml_load_string($this->response->Message->any, $this->getXmlResponseClass());
+    }
+
+    public function getTypeMessage(): string
+    {
+        return 'GetTicket';
+    }
+
+    public function getXmlResponseClass(): string
+    {
+        return GetTicketXmlResponse::class;
+    }
+
+    public function getResponse(): ResponseSendMessage
+    {
+        return $this->xmlResponse;
+    }
+
+    public function isProcessFinished(): bool
+    {
+        return $this->isCompleted($this->response);
+    }
+
+    public function executeRequest(): void
+    {
+        $this->response = $this->client->__soapCall("GetMessage", $this->getXml());
     }
 }
--- src/GetMessage/TimeoutStrategies/ExponentialBackoff.php
+++ src/GetMessage/TimeoutStrategies/ExponentialBackoff.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Fns\GetMessage\TimeoutStrategies;
 
+use Exception;
 use Fns\Contracts\RequestsManager;
 use Fns\Contracts\TimeoutStrategyHandler;
 
@@ -13,8 +14,8 @@ class ExponentialBackoff implements TimeoutStrategyHandler
      */
     private $manager;
 
-    private $expMinDelayMicSeconds = 400;
-    private $expMaxDelayMicSeconds = 60000;
+    private $expMinDelayMicroSeconds = 500000;
+    private $expMaxDelayMicroSeconds = 60000000;
     private $expFactor = 2.71828;
     private $expJitter = 0.1;
 
@@ -25,17 +26,17 @@ class ExponentialBackoff implements TimeoutStrategyHandler
 
     public function handleTimeout()
     {
-        $delay = $this->expMinDelayMicSeconds;
+        $delay = $this->expMinDelayMicroSeconds;
 
         while (true) {
             $this->manager->executeRequest();
             if ($this->manager->isProcessFinished()) {
                 break;
             }
-            dump((int)$delay);die();
+            echo $delay . PHP_EOL;
             usleep((int)$delay);
             $delay = $delay * $this->expFactor;
-            if ($delay > $this->expMaxDelayMicSeconds) {
+            if ((int)$delay > $this->expMaxDelayMicroSeconds) {
                 throw new Exception('Timeout on server side expired');
 //                $delay = $expMaxDelay;
             }
