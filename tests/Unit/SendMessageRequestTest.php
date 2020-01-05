9bfbdf31e4d3a1d68515a9e3be0f9b6e03a351db
agoalofalife <agoalofalife@gmail.com>
2020-01-05 10:44:22 +0300

    easy test SendMessageRequest

---
+++ tests/Unit/SendMessageRequestTest.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use Fns\ClientSoap;
+use Fns\GetMessage\GetMessageRequest;
+use Fns\SendMessageRequest;
+use Fns\Ticket;
+use SoapClient;
+use Tests\TestCase;
+
+class SendMessageRequestTest extends TestCase
+{
+    public function testExecute()
+    {
+        $randomMessageId = $this->randomString(5);
+        $mockTicket = $this->mock(Ticket::class);
+        $mockTicket->shouldReceive('asXml')->once();
+
+        $mockNativeClientSoap = $this->mock(SoapClient::class);
+        $mockClient = $this->mock(ClientSoap::class);
+
+        $mockClient->shouldReceive('getClient')->once()->andReturn($mockNativeClientSoap);
+        $mockNativeClientSoap->shouldReceive('__soapCall')
+            ->once()
+            ->andReturn((object)['MessageId' => $randomMessageId]);
+
+
+        $mockGetMessageRequest = $this->mock(GetMessageRequest::class);
+        $mockGetMessageRequest->shouldReceive('setClient')->with($mockClient);
+        $mockGetMessageRequest->shouldReceive('getTypeMessage');
+        $mockGetMessageRequest->shouldReceive('setMessageId')->with($randomMessageId);
+        $mockGetMessageRequest->shouldReceive('send')->once();
+        $mockGetMessageRequest->shouldReceive('getResponse')->once();
+
+        $sender = new SendMessageRequest($mockClient, $mockGetMessageRequest);
+        $sender->setTicket($mockTicket);
+        $sender->execute();
+    }
+}
